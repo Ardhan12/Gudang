0f0484662b5e1fe0d18172ed80ee3b13ea0fb7fa
illiyyaa <43401946+illiyyaa@users.noreply.github.com>
2020-12-17 00:34:20 +0700

    update tampilan superadm

--- application/views/super_admin/spradm_home_page.php
+++ application/views/super_admin/spradm_home_page.php
@@ -1,5 +1,5 @@
  <!-- MAIN CONTENT-->
- <div class="main-content">
+ <div class="main-content" style="background-color:#E4CEA3">
      <div class="section__content section__content--p30">
          <div class="container-fluid">
              <div class="row">
@@ -36,40 +36,9 @@
                                              <span>Admin Terdaftar</span>
                                          </div>
                                      </div>
-                                     <!--<div class="overview-chart">
-                                            <canvas id="widgetChart3"></canvas>
-                                        </div>-->
                                  </div>
                              </div>
                              <img style="width:200%;" src="<?= base_url() ?>assets/images/banner/Carousel1.png">
-                             <!--<h3 class="title-2">recent reports</h3>
-                                        <div class="chart-info">
-                                            <div class="chart-info__left">
-                                                <div class="chart-note">
-                                                    <span class="dot dot--blue"></span>
-                                                    <span>products</span>
-                                                </div>
-                                                <div class="chart-note mr-0">
-                                                    <span class="dot dot--green"></span>
-                                                    <span>services</span>
-                                                </div>
-                                            </div>
-                                            <div class="chart-info__right">
-                                                <div class="chart-statis">
-                                                    <span class="index incre">
-                                                        <i class="zmdi zmdi-long-arrow-up"></i>25%</span>
-                                                    <span class="label">products</span>
-                                                </div>
-                                                <div class="chart-statis mr-0">
-                                                    <span class="index decre">
-                                                        <i class="zmdi zmdi-long-arrow-down"></i>10%</span>
-                                                    <span class="label">services</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="recent-report__chart">
-                                            <canvas id="recent-rep-chart"></canvas>
-                                        </div>-->
                          </div>
                      </div>
                  </div>
