d0fc83eb7184ca5d530af7528cf4bd74b67c631a
tivanshaker <43400950+tivanshaker@users.noreply.github.com>
2020-12-16 20:11:36 +0700

    edit files

--- application/views/templates/usersTemplates/footer.php
+++ application/views/templates/usersTemplates/footer.php
@@ -62,19 +62,19 @@
   <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="<?= base_url()?>Web_Statis/assets/vendor/jquery/jquery.min.js"></script>
-  <script src="<?= base_url()?>Web_Statis/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="<?= base_url()?>Web_Statis/assets/vendor/jquery.easing/jquery.easing.min.js"></script>
-  <script src="<?= base_url()?>Web_Statis/assets/vendor/php-email-form/validate.js"></script>
-  <script src="<?= base_url()?>Web_Statis/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="<?= base_url()?>Web_Statis/assets/vendor/venobox/venobox.min.js"></script>
-  <script src="<?= base_url()?>Web_Statis/assets/vendor/waypoints/jquery.waypoints.min.js"></script>
-  <script src="<?= base_url()?>Web_Statis/assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="<?= base_url()?>assets/vendor/jquery/jquery.min.js"></script>
+  <script src="<?= base_url()?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?= base_url()?>assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="<?= base_url()?>assets/vendor/php-email-form/validate.js"></script>
+  <script src="<?= base_url()?>assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="<?= base_url()?>assets/vendor/venobox/venobox.min.js"></script>
+  <script src="<?= base_url()?>assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="<?= base_url()?>assets/vendor/owl.carousel/owl.carousel.min.js"></script>
 
 
 
   <!-- Template Main JS File -->
-  <script src="<?= base_url()?>Web_Statis/assets/js/main.js"></script>
+  <script src="<?= base_url()?>assets/js/main.js"></script>
 
   <!-- Template Main JS File -->
   <script src="<?= base_url()?>assets/js/main.js"></script>
--- application/views/templates/usersTemplates/header.php
+++ application/views/templates/usersTemplates/header.php
@@ -46,25 +46,25 @@
   <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 
   <!-- Favicons -->
-  <!--<link href="Web_Statis/assets/img/favicon.png" rel="icon">
-  <link href="Web_Statis/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <!--<link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="<?= base_url()?>Web_Statis/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="<?= base_url()?>Web_Statis/assets/vendor/icofont/icofont.min.css" rel="stylesheet">
-  <link href="<?= base_url()?>Web_Statis/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="<?= base_url()?>Web_Statis/assets/vendor/animate.css/animate.min.css" rel="stylesheet">
-  <link href="<?= base_url()?>Web_Statis/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="<?= base_url()?>Web_Statis/assets/vendor/venobox/venobox.css" rel="stylesheet">
-  <link href="<?= base_url()?>Web_Statis/assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="<?= base_url()?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?= base_url()?>assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="<?= base_url()?>assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="<?= base_url()?>assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="<?= base_url()?>assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="<?= base_url()?>assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="<?= base_url()?>assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="<?= base_url()?>Web_Statis/assets/css/style.css" rel="stylesheet">
-  <link rel="stylesheet" href="<?= base_url()?>web_statis/assets/css/font-awesome.css">
-  <link rel="stylesheet" href="<?= base_url()?>Web_Statis/assets/css/font-awesome.min.css">
+  <link href="<?= base_url()?>assets/css/style.css" rel="stylesheet">
+  <link rel="stylesheet" href="<?= base_url()?>assets/css/font-awesome.css">
+  <link rel="stylesheet" href="<?= base_url()?>assets/css/font-awesome.min.css">
 
  
 </head>
